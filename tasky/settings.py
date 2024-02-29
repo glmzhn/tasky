0a5473ac5b3baf2255f50b9fc8cd78c04e7b5e91
glmzhn <126910400+glmzhn@users.noreply.github.com>
2024-02-29 19:36:35 +0500

    Update settings.py

--- tasky/settings.py
+++ tasky/settings.py
@@ -133,9 +133,9 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'galymzhan2007@gmail.com'
+EMAIL_HOST_USER = ''
 
-EMAIL_HOST_PASSWORD = 'Galym567'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_USE_TLS = True
 
